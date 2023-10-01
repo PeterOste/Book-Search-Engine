6012f3e726fc77d4d24d461f77c7b4f941ef154b
unknown <peter.ostertag99@gmail.com>
2023-10-01 14:38:11 -0500

    Add query to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,15 @@ const { User, Book } = require('../models');
 
 const resolvers = {
     Query: {
+        me: async (_, __, context) => {
+            // Check user authentication
+            if (!context.user) {
+                throw new AuthenticationError('Not logged in');
+            }
 
+            // Return authenticated user
+            return await User.findById(context.user._id).populate('savedBooks');
+        },
     },
     Mutation: {
 
