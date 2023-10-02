291668495978461174cd6bdbf730fd97ccd51a53
unknown <peter.ostertag99@gmail.com>
2023-10-02 11:30:57 -0500

    Add LOGIN_USER mutation

--- client/src/graphql/mutations.js
+++ client/src/graphql/mutations.js
@@ -1,1 +1,26 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql `
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql `
+
+`;
+
+export const SAVE_BOOK = gql `
+
+`;
+
+export const REMOVE_BOOK = gql `
+
+`;
