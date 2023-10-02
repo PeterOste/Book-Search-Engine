89201944c19e7a3bcd975a2a4e53c72560bf00f6
unknown <peter.ostertag99@gmail.com>
2023-10-02 13:45:17 -0500

    Add saveBook

--- client/src/graphql/mutations.js
+++ client/src/graphql/mutations.js
@@ -26,7 +26,20 @@ export const ADD_USER = gql `
 `;
 
 export const SAVE_BOOK = gql `
-
+    mutation saveBook($input: BookInput!) {
+        saveBook(input: $input) {
+            _idusernameusername
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
 `;
 
 export const REMOVE_BOOK = gql `
