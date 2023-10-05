da262701ce3024a8c0dbe3abd4bad14b7ac191bb
unknown <peter.ostertag99@gmail.com>
2023-10-04 19:36:19 -0500

    Redeploy

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
